57c3b9817634f2190b6ef5d9d03a143061e751f9
Fabian Wieczorek <fabian.wieczorek@dlr.de>
2025-02-17 15:43:13 +0100

    Added Ros2ColorCamera; RunTrackerProcess shows images

--- include/obj_track_ros/obj_track_ros_node.hpp
+++ include/obj_track_ros/obj_track_ros_node.hpp
@@ -8,11 +8,38 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "cv_bridge/cv_bridge.hpp"
 
 #include <m3t/tracker.h>
+#include <m3t/generator.h>
 
 namespace obj_track_ros
 {
+
+class Ros2ColorCamera : public m3t::ColorCamera {
+  public:
+    Ros2ColorCamera(const std::string & name) : ColorCamera(name) {}
+    bool is_ready = false;
+
+    bool SetUp() override
+    {
+      set_up_ = true;
+      return true;
+    }
+    
+    bool UpdateImage(bool synchronized) override
+    {
+      std::cout << "update image" << std::endl; 
+      return true;
+    }
+
+    void setImage(cv::Mat img)
+    {
+      image_ = img;
+      is_ready = true;
+    }
+};
 
 class ObjTrackRosNode : 
   public rclcpp::Node, 
@@ -24,12 +51,16 @@ class ObjTrackRosNode :
     bool UpdatePublisher(int iteration) override;
     bool UpdateSubscriber(int iteration) override;
     bool SetUp() override;
+    std::shared_ptr<Ros2ColorCamera> camera_color;
 
   private:
     void timer_callback();
+    void receive_image(const sensor_msgs::msg::Image::SharedPtr msg);
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub;
     size_t count_;
+
 };
 
 }
--- src/obj_track_ros_node.cpp
+++ src/obj_track_ros_node.cpp
@@ -1,37 +1,47 @@
 #include "obj_track_ros/obj_track_ros_node.hpp"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 namespace obj_track_ros
 {
-  ObjTrackRosNode::ObjTrackRosNode() : Node("minimal_publisher"), Publisher("minimal_publisher"), Subscriber("minimal_publisher"), count_(0)
+
+
+  ObjTrackRosNode::ObjTrackRosNode() : Node("obj_track_ros"), Publisher("obj_track_ros"), Subscriber("obj_track_ros"), count_(0)
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    img_sub = this->create_subscription<sensor_msgs::msg::Image>("ee_camera_color", 10, std::bind(&ObjTrackRosNode::receive_image, this, _1));
     timer_ = this->create_wall_timer(500ms, std::bind(&ObjTrackRosNode::timer_callback, this));
+    camera_color = std::make_shared<obj_track_ros::Ros2ColorCamera>("color_camera");
   }
 
   void ObjTrackRosNode::timer_callback()
   {
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
+  }
+
+  void ObjTrackRosNode::receive_image(const sensor_msgs::msg::Image::SharedPtr msg) 
+  {
+    auto cvim = cv_bridge::toCvCopy(msg, msg->encoding);
+    camera_color->setImage(cvim->image);
   }
 
   bool ObjTrackRosNode::UpdatePublisher(int iteration)
   {
     rclcpp::spin_some(shared_from_this());
-    return false;
+    return true;
   }
 
   bool ObjTrackRosNode::SetUp()
   {
-    return false;
+    return true;
   }
 
   bool ObjTrackRosNode::UpdateSubscriber(int iteration)
   {
-    return false;
+    return true;
   }
 }
 
@@ -39,8 +49,42 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   std::shared_ptr<obj_track_ros::ObjTrackRosNode> node = std::make_shared<obj_track_ros::ObjTrackRosNode>();
-  std::shared_ptr<m3t::Tracker> tracker;
+  
+  RCLCPP_INFO(node->get_logger(), "Wait for image message...");
+  rclcpp::GenericRate rate(100.0);
+  while(rclcpp::ok() && !node->camera_color->is_ready)
+  {
+    rclcpp::spin_some(node->shared_from_this());
+    rate.sleep();
+  }
+  
+  auto tracker{std::make_shared<m3t::Tracker>("tracker")};
+
+  const std::filesystem::path configfile_path {
+    "/home/wiec_fa/ws/src/obj_track_ros/submodules/3DObjectTracking/M3T/data/pen_paper_demo/config.yaml"
+  };
+  
+  auto color_viewer{std::make_shared<m3t::ImageColorViewer>("color_viewer", node->camera_color)};
+
+  tracker->AddViewer(color_viewer);
+  // if (!GenerateConfiguredTracker(configfile_path, &tracker)) {
+  //   rclcpp::shutdown();
+  //   return -1;
+  // }
+
   tracker->AddPublisher(node);
+  if (!tracker->SetUp()) 
+  {
+    rclcpp::shutdown();
+    return -1;
+  };
+  if (!tracker->RunTrackerProcess(false, false))
+  {
+    rclcpp::shutdown();
+    return -1;
+  }
+
+  
   rclcpp::shutdown();
   return 0;
 }
